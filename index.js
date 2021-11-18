c426673acca5f1c279137af5f434068af07f7889
GLBryan <garrettleebryan@gmail.com>
2021-11-17 22:21:17 -0500

    removed query.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const cTable = require("console.table")
 const inq = require("inquirer")
 const mysql = require("mysql2")
-const runQuery = require("./assets/query")
-const query = require("./assets/query")
 
-function menu(){
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'password123',      
+    database: 'employees_db'
+  }
+);
+
+function runMenu(){
   inq
   .prompt(
     {
@@ -18,38 +25,27 @@ function menu(){
   .then((res) => {
     switch(res.menu) {
       case "view all departments":
-        runQuery("vad")
-        console.log('\n')
+        vad()
         break;
         
       case "view all roles":
-        runQuery("var")
+        vAr()
         break;
 
       case "view all employees":
-        runQuery("vae")
+        vae()
         break;
 
       case "add a department":
-        inq
-        .prompt(
-          {
-            type: 'input',
-            message: 'Enter department name.',
-            name: 'department'
-          })
-          .then(res => {
-            let value = res.department
-            runQuery("aad",value)
-          })
+        aad()
         break;
 
       case "add a role":
-        runQuery("aar")
+        aar()
         break;
 
       case "add an employee":
-        runQuery("aae")
+        aae()
         break;
 
       case "update an employee role":
@@ -58,9 +54,200 @@ function menu(){
       
       case "close":
         console.log('program closed')
+        process.exit()
         break
     }
   })
 }
 
-menu()
+function vad() {
+  db.query("SELECT * FROM department", function (err, results) {
+    console.table(results)
+    runMenu()
+  })
+}
+
+function vAr() {
+  db.query(`SELECT role.id, title, salary, department_name 
+            FROM role JOIN department ON role.department_id = department.id`,
+
+    function (err, results) {
+      console.table(results);
+      runMenu()
+  });
+}
+
+function vae() {
+  db.query(`SELECT emp.id, emp.first_name, emp.last_name, title, salary, department_name, 
+            CONCAT(emp2.first_name, ' ', emp2.last_name) AS "manager_name" FROM employee AS emp
+            JOIN role ON emp.role_id = role.id
+            JOIN department ON role.department_id = department.id 
+            LEFT JOIN employee AS emp2 ON emp.manager_id = emp2.id;`, 
+    function (err, results) {
+      console.table(results);
+      runMenu()
+  });
+}
+
+function aad() {
+  inq
+    .prompt(
+      {
+        type: 'input',
+        message: 'Enter department name.',
+        name: 'department'
+      })
+      .then(res => {
+        let value = res.department
+        db.query(`INSERT INTO department(department_name) VALUES (?)`, value, function (err) {
+          if (err) {
+            console.log(err);
+          }
+          console.log("Department added.")
+          runMenu()
+        });
+      })
+}
+
+function aar() {
+  db.query('SELECT department_name FROM department', function (err, results) {
+    let departments = []
+
+    for (let i of results) {
+      departments.push(i.department_name)
+    }
+
+    inq
+      .prompt([
+        {
+          type: 'input',
+          message: 'Enter role title.',
+          name: 'title'
+        },
+        {
+          type: 'input',
+          message: 'Enter role salary.',
+          name: 'salary'
+        },
+        {
+          type: 'list',
+          message: 'Choose department.',
+          name: "department",
+          choices: departments
+        }
+      ])
+      .then(res => {
+        const {title, salary, department} = res
+
+        db.query(`SELECT id FROM department WHERE department_name = ?`, department, function (err, res) {
+          if (err) {
+            console.log(err);
+          }
+
+          let dptId = res[0].id
+    
+          db.query(`INSERT INTO role (title, salary, department_id)
+                    VALUES (?,?,?)`, [title, salary, dptId], function (err) {
+            if (err) {
+              console.log(err);
+            }
+            console.log("Role added.")
+            runMenu()
+          });
+        });
+      })
+  });
+}
+
+function aae() {
+  db.query('SELECT title FROM role', function (err, results) {
+    let roles = []
+
+    for (let i of results) {
+      roles.push(i.title)
+    }
+
+    db.query("SELECT CONCAT(first_name, ' ', last_name) AS 'manager_name' FROM employee", function (err, results) {
+      let employees = []
+
+      for (let i of results) {
+        employees.push(i.manager_name)
+      }
+      employees.push("none")
+
+      inq
+        .prompt([
+        {
+          type: 'input',
+          message: "Enter employee's first name.",
+          name: 'fname'
+        },
+        {
+          type: 'input',
+          message: "Enter employee's last name.",
+          name: 'lname'
+        },
+        {
+          type: 'list',
+          message: "Choose employee's role.",
+          name: 'role',
+          choices: roles
+        },
+        {
+          type: 'list',
+          message: "Choose employee's manager.",
+          name: 'manager',
+          choices: employees
+        }
+        ])
+        .then(res => {
+          const {fname, lname, role, manager} = res
+
+          db.query(`SELECT id FROM role WHERE title = ?`, role, function (err, res) {
+            if (err) {
+              console.log(err);
+            }
+
+            let roleId = res[0].id
+            
+            if (manager === "none"){
+              var managerId = null
+              
+              db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                        VALUES (?,?,?,?)`, [fname, lname, roleId, managerId], function (err) {
+                if (err) {
+                  console.log(err);
+                }
+                console.log("Employee added.")
+                runMenu()
+              })
+            } else {
+              let split = manager.split(" ")
+
+              db.query(`SELECT id FROM employee WHERE first_name = ? AND last_name = ?`, [split[0], split[1]], function (err, res) {
+                if (err) {
+                  console.log(err);
+                }
+  
+                let managerId = res[0].id
+                
+                db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                          VALUES (?,?,?,?)`, [fname, lname, roleId, managerId], function (err) {
+                  if (err) {
+                    console.log(err);
+                  }
+
+                  console.log("Employee added.")
+                  runMenu()
+                })
+              })
+            }
+          })
+        })
+    })
+  })
+}
+
+runMenu()
+
+module.exports = runMenu
